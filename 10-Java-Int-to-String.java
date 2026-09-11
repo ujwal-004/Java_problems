1e9678bbbfe54fbb5025f8c3654190e7f75c36ee
ujwal-004 <deepujwal33@gmail.com>
2026-09-11 17:23:06 +0530

    Delete 10-Java-Int-to-String.java

--- 10-Java-Int-to-String.java
+++
@@ -1,7 +0,0 @@
-public class Solution {
-    public static void main(String[] args) {
-        int n = 130;
-        String s = String.valueOf(n) + " is the value of n";
-        System.out.println(s);
-    }
-}
